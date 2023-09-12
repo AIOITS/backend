5b00f70e036ce26bd623f3a4058c3d57466e2834
Fernando Sibarani <fernandosibarani45@gmail.com>
2023-09-12 18:46:47 +0700

    fix: fix bug data type

--- src/@generated/prisma-nestjs-graphql/prisma/enum-status-pengajuan-subsidi-field-update-operations.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql'
-import { InputType } from '@nestjs/graphql'
-import { StatusPengajuanSubsidi } from './status-pengajuan-subsidi.enum'
-
-@InputType()
-export class EnumStatusPengajuanSubsidiFieldUpdateOperationsInput {
-  @Field(() => StatusPengajuanSubsidi, { nullable: true })
-  set?: keyof typeof StatusPengajuanSubsidi
-}
